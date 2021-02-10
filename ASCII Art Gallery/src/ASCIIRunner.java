593e9190dbf7d7b6f7165f93d173819be7967df2
amanda.muhr <amanda.muhr@mu-c1-03.mullen.pvt.k12.co.us>
2021-02-10 09:51:52 -0700

    Created brief intro to situate user

--- ASCII Art Gallery/src/ASCIIRunner.java
+++ ASCII Art Gallery/src/ASCIIRunner.java
@@ -4,8 +4,20 @@ public class ASCIIRunner
 
 		public static void main(String[] args)
 			{
-				System.out.println("Hello world");
+				// Goals:
+				// 1) Let user choose color they want to see the ASCII art in
+				// 2) Have at least 3 categories of pictures for them to look through
 
+				greetUser();
+			}
+		
+		public static void greetUser()
+			{
+				System.out.println("Welcome to the ASCII Art Gallery!");
+				System.out.println("We are the only institution in the APCS world that has exhibits made entirely of ASCII art.");
+				System.out.println(" ");
+				//Choose color
+				System.out.println("Before we begin, please choose the color you'd like to view the pictures in:");
 			}
 
 	}
