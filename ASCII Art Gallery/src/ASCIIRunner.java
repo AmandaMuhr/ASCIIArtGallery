56433ece04a976cc153317f8b083d03abb8ab0b0
amandamuhr <amandamuhr@localhost>
2021-02-11 10:12:45 -0700

    NOT WORTH MERGING YET

--- ASCII Art Gallery/src/ASCIIRunner.java
+++ ASCII Art Gallery/src/ASCIIRunner.java
@@ -9,6 +9,7 @@ public class ASCIIRunner
 				// 2) Have at least 3 categories of pictures for them to look through
 
 				greetUser();
+				endTour();
 			}
 		
 		public static void greetUser()
@@ -19,14 +20,27 @@ public class ASCIIRunner
 				//Choose color
 				System.out.println("Before we begin, please choose the color you'd like to view the pictures in:");
 				
-				//Printing red in red not working like before
-				System.out.println("1)" +  "\u001b[1;31mred\u001b[0m");
+				//Printing "red" in red not working like before
+				//The following are a bunch of print statements trying to make it work
+				
+				System.out.println(ConsoleColors.RED + "RED COLORED" +
+						ConsoleColors.RESET);
+				System.out.println(ConsoleColors.RED_BOLD + "this is red");
 			}
 		
 		public static void testPlugIn()
-		{
-			//Confirmed that color plug in works
-			System.out.println("Hello \u001b[1;31mred\u001b[0m world!");
-		}
+			{
+				//Confirmed that color plug in works
+				//Immediately after adding ConsoleColors class,it's no longer working
+				System.out.println("Hello \u001b[1;31mred\u001b[0m world!");
+			}
+		
+		public static void endTour()
+			{
+				System.out.println(" ");
+				System.out.println("Welcome back! We hope you enjoyed your time at the ASCII Art Gallery!");
+				System.out.println("Special thanks go to Chris Moser for his patience & computer wizardry.");
+			}
+		
 
 	}
