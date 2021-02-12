6bc4bcd9ba0ac6e533a68b04a3d4fd49f2db9324
amanda.muhr <amanda.muhr@mu-c1-03.mullen.pvt.k12.co.us>
2021-02-12 09:56:26 -0700

    Choosing exhibits framework established

--- ASCII Art Gallery/src/ASCIIRunner.java
+++ ASCII Art Gallery/src/ASCIIRunner.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class ASCIIRunner
 	{
 
+		static Scanner userInput = new Scanner(System.in);
+		static int choice = 0;
+		
 		public static void main(String[] args)
 			{
 				// Goals:
@@ -11,6 +14,8 @@ public class ASCIIRunner
 				// 3) If time, add a randomized color option
 
 				greetUser();
+				chooseExhibit();
+				chooseColor();
 				endTour();
 			}
 		
@@ -19,13 +24,16 @@ public class ASCIIRunner
 				//Example of how to color text
 				//System.out.println(ConsoleColors.RED + "RED COLORED" + ConsoleColors.RESET);
 			
-				Scanner userInput = new Scanner(System.in);
 				
 				System.out.println("Welcome to the ASCII Art Gallery!");
 				System.out.println("We are the only institution in the APCS world that has exhibits made entirely of ASCII art.");
 				System.out.println(" ");
-				
+			}
+		
+		public static void chooseColor()
+		{
 				//Choose color
+				System.out.println(" ");
 				System.out.println("Before we begin, please choose the color you'd like to view the pictures in:");
 				System.out.println("1) " + ConsoleColors.RED + "Red" + ConsoleColors.RESET);
 				System.out.println("2) " + ConsoleColors.GREEN + "Green" + ConsoleColors.RESET);
@@ -33,7 +41,7 @@ public class ASCIIRunner
 				System.out.println("4) " + ConsoleColors.PURPLE + "Purple" + ConsoleColors.RESET);
 				System.out.println("5) " + ConsoleColors.CYAN + "Cyan" + ConsoleColors.RESET);
 				//It doesn't like 'private' for some reason???
-				int choice = userInput.nextInt();
+				choice = userInput.nextInt();
 				
 				if(choice == 1)
 					{
@@ -62,6 +70,17 @@ public class ASCIIRunner
 				
 			}
 		
+		public static void chooseExhibit()
+			{
+				//Just beginning ideas for categories; can change up until ASCII images are uploaded
+				System.out.println("Which exhbit would you like to view?");
+				System.out.println("1) Nature");
+				System.out.println("2) Superheroes");
+				System.out.println("3) Patriotism");
+				choice = userInput.nextInt();
+			}
+	
+		
 		public static void testPlugIn()
 			{
 				//Confirmed that color plug in works on both machines
@@ -70,34 +89,44 @@ public class ASCIIRunner
 		
 		public static void viewInRed()
 			{
-			
+				System.out.println(ConsoleColors.RED + " ");
+				//Run method here
+				System.out.println(ConsoleColors.RESET + " ");
 			}
 		
 		public static void viewInGreen()
 			{
-			
+				System.out.println(ConsoleColors.GREEN + " ");
+				//Run method here
+				System.out.println(ConsoleColors.RESET + " ");
 			}
 		
 		public static void viewInBlue()
 			{
-
+				System.out.println(ConsoleColors.BLUE + " ");
+				//Run method here
+				System.out.println(ConsoleColors.RESET + " ");
 			}
 		
 		public static void viewInPurple()
 			{
-			
+				System.out.println(ConsoleColors.PURPLE + " ");
+				//Run method here
+				System.out.println(ConsoleColors.RESET + " ");
 			}
 		
 		public static void viewInCyan()
 			{
-			
+				System.out.println(ConsoleColors.CYAN + " ");
+				//Run method here
+				System.out.println(ConsoleColors.RESET + " ");
 			}
 			
 		public static void endTour()
 			{
-				System.out.println(" ");
 				System.out.println("Welcome back! We hope you enjoyed your time at the ASCII Art Gallery!");
-				System.out.println("Special thanks go to Chris Moser for his patience & computer wizardry.");
+				System.out.println(" ");
+				System.out.println("Note from Creator: Special thanks go to Chris Moser for his patience & computer wizardry.");
 			}
 		
 
